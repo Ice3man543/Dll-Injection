c41cea4fb6eba1eeea6d9258fa0baa6cdd668e5d
Nizamul Rana <nizamulrana@gmail.com>
2018-04-23 07:06:38 -0700

    Created dllmain.c

---
+++ TestDll/dllmain.c
@@ -0,0 +1,26 @@
+#include <windows.h>
+
+BOOL APIENTRY DllMain(HINSTANCE hInstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+    switch (fdwReason)
+    {
+        case DLL_PROCESS_ATTACH:
+			MessageBoxA(NULL, "Hello From The Injected DLL", "Injected !", MB_OK | MB_ICONINFORMATION);
+            break;
+
+        case DLL_THREAD_ATTACH:
+			MessageBoxA(NULL, "Hello From The Injected DLL", "Injected !", MB_OK | MB_ICONINFORMATION);
+            break;
+
+        case DLL_THREAD_DETACH:
+			MessageBoxA(NULL, "Hello Again From The Injected DLL", "Injected !", MB_OK | MB_ICONINFORMATION);
+            break;
+
+        case DLL_PROCESS_DETACH:
+			MessageBoxA(NULL, "Hello Again From The Injected DLL", "Injected !", MB_OK | MB_ICONINFORMATION);
+            break;
+    }
+
+    /* Return success */
+    return TRUE;
+}
